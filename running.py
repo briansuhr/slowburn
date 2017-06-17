2ec1975da12cb9d95b1e1db7820f30850e075e4e
Brian Suhr <brian@briansuhr.com>
2017-06-17 16:51:37 -0700

    Add sample Darksky API call

--- running.py
+++ running.py
@@ -1,3 +1,15 @@
 import tcxparser
+from darksky import forecast
+from configparser import ConfigParser
+
+# Darksky weather API.
+# Create config file manually
+parser = ConfigParser()
+parser.read('slowburn.config', encoding='utf-8')
+darksky_key = parser.get('darksky', 'key')
+
 tcx = tcxparser.TCXParser('gps_logs/2017-06-15_Running.tcx')
+
 print(tcx.duration)
+boston = forecast(darksky_key, 42.3601, -71.0589)
+print(boston)
