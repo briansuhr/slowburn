4d2c77a10904986834caedfeb7378a24e7cf66a6
Brian Suhr <brian@briansuhr.com>
2017-08-06 17:40:12 -0700

    Add another assert test

--- tests/test_running.py
+++ tests/test_running.py
@@ -7,6 +7,7 @@ from slowburn.running import convert_time_to_unix
 class MyTest(unittest.TestCase):
     def test_convert_time_to_unix(self):
         self.assertEqual(convert_time_to_unix("2017-06-15T19:01:17.000Z"), '1497553277')
+        self.assertEqual(convert_time_to_unix("2012-04-01T04:59:22.000Z"), '1333256362')
 
 
 if __name__ == '__main__':
