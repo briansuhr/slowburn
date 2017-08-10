b35d38664d323597671cf72b125deb74eb9d5473
Brian Suhr <brian@briansuhr.com>
2017-08-09 21:00:40 -0700

    Move local_time and get_timezone out of GetWeather Darksky API does timezone conversion automatically.

--- slowburn/running.py
+++ slowburn/running.py
@@ -22,16 +22,29 @@ def read_all_gps_files(gps_logs_directory):
     for gps_file in all_gps_files:
         weather = GetWeather(gps_logs_directory + gps_file)
         print(weather.weather_type('icon'))
+        print(weather.filter_weather_type('temperature'))
         print(weather.weather_type('temperature'))
         print(weather.weather_type('humidity'))
         print(weather.weather_type('windSpeed'))
-        print(weather.local_time())
 
 
 def convert_time_to_unix(time):
     parsed_time = dateutil.parser.parse(time)
     time_in_unix = parsed_time.strftime('%s')
     return time_in_unix
+
+
+def local_time(utc_time):
+    """Convert run time from stored UTC time to the local timezone at the latitude/longitude of the run."""
+    gps_utc_time = datetime.utcfromtimestamp(float(convert_time_to_unix(utc_time)))
+    localized_utc_time = pytz.utc.localize(gps_utc_time)
+    local_timezone = pytz.timezone(self.run_timezone)
+    utc_to_local_time = localized_utc_time.astimezone(local_timezone)
+
+    return utc_to_local_time
+
+def get_timezone(latitude, longitude):
+    return tf.timezone_at(lat=latitude, lng=longitude)
 
 
 class GetWeather:
@@ -42,7 +55,6 @@ class GetWeather:
 
         self.latitude = self.tcx.latitude
         self.longitude = self.tcx.longitude
-        self.run_timezone = tf.timezone_at(lat=self.latitude, lng=self.longitude)
 
         print("Calling Darksky API...")
         self.darksky_json = self.darksky_api_request(self.run_time)
@@ -53,6 +65,7 @@ class GetWeather:
             "https://api.darksky.net/forecast/" + darksky_key + "/" + str(self.latitude) + "," + str(
                 self.longitude) + "," + convert_time_to_unix(self.run_time) + "?exclude=currently,flags").read()
         return json.loads(darksky_request.decode('utf-8'))
+
 
     def filter_weather_type(self, weather_type):
         """Get weather phenomenon data point objects (like temperature, humidity, windSpeed) for each of the 24 hours
@@ -75,14 +88,6 @@ class GetWeather:
         filtered_weather_type = (min(hours, key=lambda time_delta: time_delta[0]))[1]
         return filtered_weather_type
 
-    def local_time(self):
-        """Convert run time from stored UTC time to the local timezone at the latitude/longitude of the run."""
-        utc_time = datetime.utcfromtimestamp(float(convert_time_to_unix(self.run_time)))
-        localized_utc_time = pytz.utc.localize(utc_time)
-        local_timezone = pytz.timezone(self.run_timezone)
-        utc_to_local_time = localized_utc_time.astimezone(local_timezone)
-
-        return utc_to_local_time
 
 
 if __name__ == '__main__':
